e99d40f9356d4e1b198514d94203b226abfd3421
shina <rasheedshinaopeyemi@gmail.com>
2021-06-07 12:41:03 +0100

    added services section on the homepage

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 // import { LinkContainer } from 'react-router-bootstrap';
 import { Link } from 'react-router-dom';
 import '../styles/navbar.scss';
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Hero } from '../components/Hero';
+import Services from '../components/Services';
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <section>
+      <Hero />
+      <Services />
+    </section>
+  );
 };
 
 export default Home;
