d0d6db7240b296050d7bfc8c46339e0d8c15e076
shina <rasheedshinaopeyemi@gmail.com>
2021-06-09 18:19:09 +0100

    replaced Link component in the appbar with LinkContainer from react-router-boostrap

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,51 +1,50 @@
 import React from 'react';
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
-// import { LinkContainer } from 'react-router-bootstrap';
-import { Link } from 'react-router-dom';
+import { LinkContainer } from 'react-router-bootstrap';
 import '../styles/navbar.scss';
 import logo from '../assets/images/logo.png';
 
 const Appbar = () => {
   return (
     <Navbar style={{ marginLeft: '7rem' }} className="appbar" expand="lg">
-      <Link className="brand__logo" to="/">
+      <LinkContainer className="brand__logo" to="/">
         <Navbar.Brand href="#home">
           <img src={logo} alt="brand logo" />
         </Navbar.Brand>
-      </Link>
+      </LinkContainer>
 
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Link to="/" className="appbar__item">
+          <LinkContainer to="/" className="appbar__item">
             <Nav.Link className="appbar__link">Home</Nav.Link>
-          </Link>
-          <Link to="/" className="appbar__item">
+          </LinkContainer>
+          <LinkContainer to="/" className="appbar__item">
             <Nav.Link className="appbar__link">About Us</Nav.Link>
-          </Link>
-          <Link to="/" className="appbar__item">
+          </LinkContainer>
+          <LinkContainer to="/" className="appbar__item">
             <Nav.Link className="appbar__link">Services</Nav.Link>
-          </Link>
-          <Link to="/" className="appbar__item">
+          </LinkContainer>
+          <LinkContainer to="/" className="appbar__item">
             <Nav.Link className="appbar__link">Research</Nav.Link>
-          </Link>
-          <Link to="/" className="appbar__item">
+          </LinkContainer>
+          <LinkContainer to="/" className="appbar__item">
             <Nav.Link className="appbar__link">Download</Nav.Link>
-          </Link>
-          <Link to="/" className="appbar__item">
+          </LinkContainer>
+          <LinkContainer to="/" className="appbar__item">
             <Nav.Link className="appbar__link">Contact</Nav.Link>
-          </Link>
+          </LinkContainer>
           <NavDropdown
             className="appbar__item appbar__link"
             title="My Account"
             id="basic-nav-dropdown"
           >
-            <Link to="/">
+            <LinkContainer to="/">
               <NavDropdown.Item>Login</NavDropdown.Item>
-            </Link>
-            <Link to="/">
+            </LinkContainer>
+            <LinkContainer to="/">
               <NavDropdown.Item>Create Account</NavDropdown.Item>
-            </Link>
+            </LinkContainer>
           </NavDropdown>
         </Nav>
       </Navbar.Collapse>
