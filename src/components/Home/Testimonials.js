d8b68e4dfa38b2c34b09745ca65eee28c14edd54
shina <rasheedshinaopeyemi@gmail.com>
2021-06-18 12:40:40 +0100

    moved home hero banner and testimonial content to contentful

--- src/components/Home/Testimonials.js
+++ src/components/Home/Testimonials.js
@@ -1,37 +1,8 @@
-// import React from 'react';
-// import { testimonials } from '../../data/testimonials';
-// import Title from '../Title';
-
-// const Testimonials = () => {
-//   return (
-//     <section className="testimonials">
-//       <Title title="client testimonials" />
-//       <article className="testimonials__section">
-//         {testimonials.map((testimonial, index) => (
-//           <div className="testimonial" key={index}>
-//             <img
-//               className="mx-auto"
-//               src={testimonial.img}
-//               alt={testimonial.name}
-//             />
-//             <h5>{testimonial.testimonial}</h5>
-//             <p>- {testimonial.name}</p>
-//           </div>
-//         ))}
-//       </article>
-//     </section>
-//   );
-// };
-
-// export default Testimonials;
-
 import React, { Component } from 'react';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Slider from 'react-slick';
 import Title from '../Title';
-
-import { testimonials } from '../../data/testimonials';
 
 export default class Fade extends Component {
   render() {
@@ -44,23 +15,41 @@ export default class Fade extends Component {
       slidesToShow: 1,
       slidesToScroll: 1,
     };
+
+    const { clientsTestimonial } = this.props;
+
     return (
-      <div style={{ maxWidth: '100vw' }}>
-        <Title title="client testimonials" />
-        <Slider {...settings}>
-          {testimonials.map((testimonial, index) => (
-            <div className="testimonial" key={index}>
-              <img
-                className="mx-auto"
-                src={testimonial.img}
-                alt={testimonial.name}
-              />
-              <h5>{testimonial.testimonial}</h5>
-              <p>- {testimonial.name}</p>
-            </div>
-          ))}
-        </Slider>
-      </div>
+      <>
+        {clientsTestimonial.length === 0 ? (
+          <p>Loading Testimonail</p>
+        ) : (
+          <div style={{ maxWidth: '100vw' }}>
+            <Title title="client testimonials" />
+            <Slider {...settings}>
+              {clientsTestimonial.map((clientTestimonial, index) => {
+                const {
+                  fields: {
+                    name,
+                    testimonial,
+                    userphoto: {
+                      fields: {
+                        file: { url },
+                      },
+                    },
+                  },
+                } = clientTestimonial;
+                return (
+                  <div className="testimonial" key={index}>
+                    <img className="mx-auto" src={url} alt={name} />
+                    <h5>{testimonial}</h5>
+                    <p>- {name}</p>
+                  </div>
+                );
+              })}
+            </Slider>
+          </div>
+        )}
+      </>
     );
   }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet';
 import { Hero } from '../components/Hero';
 import LatestNews from '../components/Home/LatestNews';
@@ -8,7 +8,22 @@ import Services from '../components/Home/Services';
 import Team from '../components/Home/Team';
 import Testimonials from '../components/Home/Testimonials';
 
+import Client from '../Contentful';
+
 const Home = () => {
+  const [clientsTestimonial, setClientsTestimonial] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await Client.getEntries({
+        content_type: 'clientTestimonials',
+      });
+      setClientsTestimonial(response.items);
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <>
       <Helmet>
@@ -20,7 +35,7 @@ const Home = () => {
         <MarketPriceList />
         <Register />
         <LatestNews />
-        <Testimonials />
+        <Testimonials clientsTestimonial={clientsTestimonial} />
         <Team />
       </section>
     </>
