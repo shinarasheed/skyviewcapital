349818c88d582cc8db5b4b2b319915c5b132fdcd
shina <rasheedshinaopeyemi@gmail.com>
2021-06-29 10:26:01 +0100

    add terms and condition page

---
+++ src/components/SignupIndividual/Summary.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+import SignupTitle from '../SignupTitle';
+
+const Summary = ({ values, prevStep }) => {
+  const goBackToPreviousStep = (e) => {
+    e.preventDefault();
+    prevStep();
+  };
+
+  const handleFormSubmit = () => {
+    console.log(values);
+  };
+
+  return (
+    <>
+      <SignupTitle title="summary" />
+
+      <div>
+        <p style={{ fontSize: '1.5rem', color: '#fff' }}>
+          Mauris congue justo a ligula rutrum fermentum. Aenean posuere sit amet
+          ipsum sed ultricies. Phasellus facilisis purus vitae porta gravida.
+          Phasellus laoreet nunc ac ornare semper. Etiam fermentum diam eget ex
+          tempor tempus. Fusce arcu ex, pretium ac dictum et, euismod eu urna.
+          Donec cursus massa vulputate, molestie nibh sit amet, volutpat mi.
+          Nullam aliquam maximus placerat. Phasellus porttitor sagittis augue
+          quis aliquet. Nullam ac massa sed magna ornare consectetur. Cras
+          sagittis tempor varius. Etiam tristique ante
+        </p>
+      </div>
+
+      <Button
+        onClick={(e) => goBackToPreviousStep(e)}
+        className="mt-4"
+        variant="primary"
+        type="button"
+      >
+        Go Back
+      </Button>
+
+      <Button
+        onClick={(e) => handleFormSubmit()}
+        className="mt-4"
+        variant="primary"
+        type="button"
+      >
+        Submit
+      </Button>
+    </>
+  );
+};
+
+export default Summary;
